68b428b6af11b3d725833697966b0f05482de685
Thiago Moura <thmoura14@hotmail.com>
2023-03-17 21:31:57 -0300

    add UniqueEntityIs tests

---
+++ src/@seedwork/domain/unique-entity-id.value-object.spec.ts
@@ -0,0 +1,26 @@
+import { validate as uuidValidate } from "uuid"
+
+import { UniqueEntityId } from "./unique-entity-id.value-object"
+import { InvalidUuidError } from "../../@seedwork/errors/invalid-uuid.error"
+
+describe('UniqueEntityId Unit Tests', () => {
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
+
+  it('should throw error when uuid is invalid', () => {
+    expect(() => new UniqueEntityId('fake id')).toThrowError(InvalidUuidError)
+    expect(validateSpy).toHaveBeenCalled()
+  })
+  
+  it('should accept an uuid in constructor', () => {
+    const uuid = 'f3630b3a-8473-4917-bfe9-a3f5c4148e0a'
+    const valueObject = new UniqueEntityId(uuid)
+    expect(validateSpy).toHaveBeenCalled()
+    expect(valueObject.id).toBe(uuid)
+  })
+  
+  it('should return an uuid in constructor', () => {
+    const valueObject = new UniqueEntityId()
+    expect(validateSpy).toHaveBeenCalled()
+    expect(uuidValidate(valueObject.id)).toBeTruthy()
+  })
+})
